7805ee301e87f206873a1618cd1834dd9679a61f
kafukafienkov <bartekjmroz@gmail.com>
2014-10-25 13:30:03 +0200

    dodanie buttona z metodą zmieniającą text

--- app/src/main/java/pl/szkolenieandroid/mojaaplikacja/MyApp.java
+++ app/src/main/java/pl/szkolenieandroid/mojaaplikacja/MyApp.java
@@ -2,6 +2,8 @@ package pl.szkolenieandroid.mojaaplikacja;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 
 public class MyApp extends Activity {
@@ -9,6 +11,14 @@ public class MyApp extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.color_buttons);
+        setContentView(R.layout.new_layout);
+        TextView myTextView =(TextView) findViewById(R.id.my_text_view); //rzutowanie jawne TextView --> View
+        myTextView.setText("My text before clicking");
+    }
+
+    public void buttonAction(View view) {
+        TextView myTextView =(TextView) findViewById(R.id.my_text_view); //rzutowanie jawne TextView --> View
+        myTextView.setText("After clicking the button");
+
     }
 }
